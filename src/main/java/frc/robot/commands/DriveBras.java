0c0e95e548110d07e3ead01de548d3db485e5cfc
Huskies Prog <57733100+HuskiesProg@users.noreply.github.com>
2021-03-09 19:10:49 -0500

    command bras

---
+++ src/main/java/frc/robot/commands/DriveBras.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DriveBras extends CommandBase {
+  Double hauteur;
+  Double longueur;
+  Bras bras;
+
+
+  public DriveBras(Bras bras, Double hauteur, Double longueur) {
+    this.bras = bras;
+    this.hauteur = hauteur;
+    this.longueur = longueur;
+  }
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    bras.driveHauteur(hauteur.doubleValue());
+    bras.driveLongueur(longueur.doubleValue());
+  }
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
